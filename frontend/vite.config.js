e82192c0db7653ebb5d425b43a75c192bb319a21
EnzoBusaglia <embuscagliah@gmail.com>
2025-07-28 00:59:33 -0400

    proxy django admin view to th react fontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,10 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      "/admin": "http://localhost:8000",
+      "/static": "http://localhost:8000",
+    },
+  },
 });
