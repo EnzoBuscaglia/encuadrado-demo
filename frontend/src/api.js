f3a936c34f8f9caaee53d35b0e53df12446f5b5d
EnzoBusaglia <embuscagliah@gmail.com>
2025-07-28 11:55:35 -0400

    include new API calls for transactional operations

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -8,3 +8,8 @@ export const getEvents = () =>
   api.get("events/").then((res) => res.data.results);
 export const getContents = () =>
   api.get("contents/").then((res) => res.data.results);
+
+export const purchaseEvent = (data) =>
+  api.post("purchase/event/", data).then((res) => res.data);
+export const purchaseContent = (data) =>
+  api.post("purchase/content/", data).then((res) => res.data);
